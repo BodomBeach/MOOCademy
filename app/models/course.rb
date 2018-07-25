274b2f1b67ce08ac4339c3f3f6195958111ed5c0
Unknown <nicolas.bertin0@gmail.com>
2018-07-25 21:14:37 +0200

    added commentaries

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
   has_many :lessons
+  #Liaison des tables avec clé étrangère
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,4 @@
 class Lesson < ApplicationRecord
   belongs_to :course
+  #Liaison des tables avec clé étrangère
 end
